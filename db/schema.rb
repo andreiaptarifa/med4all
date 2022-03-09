93d6a580b6000bc13e1297ae8c996c0511aed5cf
Herman Ernst Loosli <herman.loosli@gmail.com>
2022-03-09 17:36:10 -0300

    model de pharmacy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_202147) do
+ActiveRecord::Schema.define(version: 2022_03_09_203105) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2022_03_09_202147) do
     t.string "lab"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pharmacies", force: :cascade do |t|
+    t.string "pharmacy_address"
+    t.string "pharmacy_name"
+    t.bigint "medication_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["medication_id"], name: "index_pharmacies_on_medication_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +45,5 @@ ActiveRecord::Schema.define(version: 2022_03_09_202147) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pharmacies", "medications"
 end
