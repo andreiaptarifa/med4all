ebe3c1b0786aee1f35d3b3159b1ce2afbfd65016
Laura Amaro <lauraamaro1804@gmail.com>
2022-03-11 11:07:28 -0300

    seed rodado

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,29 +3,29 @@ require 'csv'
 filepath = "db/data/new_csv.csv"
 
 CSV.foreach(filepath, quote_char: "\x00", headers: :first_row) do |row|
-
-  pharmacies = Pharmacy.new(
+  pharmacy_interior = Pharmacy.new(
     pharmacy_name: "#{row[1]}",
     pharmacy_address: "#{row[2]}, #{row[0]}"
   )
+  pharmacy_interior.save!
 end
 
 filepath_grandesp = "db/data/unidades_grandesp.csv"
 
 CSV.foreach(filepath_grandesp, headers: :first_row) do |row|
-
-  pharmacies = Pharmacy.new(
+ pharmacy_grande = Pharmacy.new(
     pharmacy_name: "#{row[2]}",
     pharmacy_address: "#{row[3]}, #{row[1]}"
   )
+  pharmacy_grande.save!
 end
 
 filepath_capital = "db/data/unidades_capital.csv"
 
 CSV.foreach(filepath_capital, headers: :first_row) do |row|
-  pharmacy = Pharmacy.new(
+  pharmacy_capital = Pharmacy.new(
     pharmacy_name: "#{row[0]}",
     pharmacy_address: "#{row[1]}, São Paulo"
   )
+  pharmacy_capital.save!
 end
-
