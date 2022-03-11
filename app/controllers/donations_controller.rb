4de108965875cd3931c499c898193774ac89ded8
michellepieroni <mipieroni@gmail.com>
2022-03-11 14:07:39 -0300

    cntroller

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,22 @@
 class DonationsController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def index
+    @donations = current_user.donations
+  end
+
+  def show
+    @donation = Donation.find(params[:id])
+  end
 end
