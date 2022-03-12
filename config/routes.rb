a9e04fcbaccf4f61738f35215c7076705c986eb1
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-11 21:08:54 -0300

    medications controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :medication_orders, only: [ :new, :create, :show, :index ]
+  resources :medication_orders, only: %i[new create show index]
 
   resources :donations, only: %i[new create index show]
+
+  resources :medications, only: :index
 end
