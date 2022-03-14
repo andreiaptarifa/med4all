f36ddbebaee5e3fa8716f8cce9d55154c892a6ce
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-14 17:35:54 -0300

    added stimulus to medication form

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -2,6 +2,9 @@ class MedicationsController < ApplicationController
   def index
     if params[:query].present?
       @medications = Medication.search_by_name_or_substance(params[:query])
+      respond_to do |format|
+        format.json {}
+      end
     else
       @medications = Medication.all
     end
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +17,7 @@ module Med4all
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Configuration for the application, engines, and railties goes here.
     #
