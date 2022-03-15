fb2faa2a9050c368611bc348aca72dd4a66a6e62
Laura Amaro <lauraamaro1804@gmail.com>
2022-03-15 08:44:51 -0300

    commiting something

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,6 @@ ActiveRecord::Schema.define(version: 2022_03_13_143830) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.string "first_name"
     t.string "last_name"
     t.string "gender"
@@ -91,6 +89,8 @@ ActiveRecord::Schema.define(version: 2022_03_13_143830) do
     t.string "cellphone"
     t.string "cpf"
     t.string "user_type"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
