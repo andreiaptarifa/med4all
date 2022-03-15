81a28bb0b44b329aacf5756b0d8cc32ff98a7dac
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-15 17:10:52 -0300

    added cloudinary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.active_storage.service = :cloudinary
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
