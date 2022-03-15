a024f287be600f419f99bf4e40bb8621974a39bf
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-15 16:20:34 -0300

    final fixes

--- db/schema.rb
+++ db/schema.rb
@@ -110,8 +110,6 @@ ActiveRecord::Schema.define(version: 2022_03_15_145051) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "latitude"
-    t.float "longitude"
     t.string "first_name"
     t.string "last_name"
     t.string "gender"
@@ -119,6 +117,8 @@ ActiveRecord::Schema.define(version: 2022_03_15_145051) do
     t.string "cellphone"
     t.string "cpf"
     t.string "user_type"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
