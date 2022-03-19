7543d11daf4ae7deda7648fa9142c8cfaad7a965
andreia tarifa <andreia.p.tarifa@gmail.com>
2022-03-19 17:52:09 -0300

    changes in session

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-
   resources :medication_orders, only: %i[new create show index]
   resources :donations, only: %i[new create index show]
   resources :pharmacies, only: :index
